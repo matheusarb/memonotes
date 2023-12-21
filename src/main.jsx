848757c637a3509ecfbdcfc43da3e46e7ef06d31
Matheus <mat.araujoribeiro@gmail.com>
2023-12-20 22:27:55 -0300

    [add] themeprovider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Index } from './pages/Details/Index.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { Details } from "./pages/Details/Index.jsx";
+import theme from "./styles/theme.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Index />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>
+);
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -2,5 +2,4 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     background: red;
-    padding: 1.2rem;
 `;
