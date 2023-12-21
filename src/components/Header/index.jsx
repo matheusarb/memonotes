d5b608690ab40becfe94ac3e71f7c26014ff44c1
Matheus <mat.araujoribeiro@gmail.com>
2023-12-21 13:52:54 -0300

    [add] header and profile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,19 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
     return (
         <Container>
-            
+            <Profile>
+                <img 
+                    src="http://github.com/matheusarb.png"
+                    alt="User image"
+                />
+
+                <div>
+                    <span>Welcome</span>
+                    <strong>Username</strong>
+                </div>
+            </Profile>
         </Container>        
     )
 }
