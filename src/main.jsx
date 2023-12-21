cd16d4324fd287324ffca049cee0b5d757e830e6
Matheus <mat.araujoribeiro@gmail.com>
2023-12-20 23:45:02 -0300

    [add] global themes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
-
-import { Details } from "./pages/Details/Index.jsx";
+import GlobalStyles from './styles/global.js';
+import { Index } from "./pages/Details/Index.jsx";
 import theme from "./styles/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Details />
+      <GlobalStyles />
+      <Index />
     </ThemeProvider>
   </React.StrictMode>
 );
